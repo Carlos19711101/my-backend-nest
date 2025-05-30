817205284f1e6791b6c3e3c92b93f1a40a51559b
carlos <carlos.velez@est.iudigital.edu.co>
2025-05-30 18:49:02 -0500

    create app.module.ts

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+// Importa tus módulos (UsersModule, AuthModule, etc.)
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    // Conexión a MongoDB (cambia la URI por la de tu base de datos)
+    MongooseModule.forRoot('mongodb://localhost/nest-auth-db'),
+
+    // Módulos de tu aplicación
+    UsersModule,
+    AuthModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
